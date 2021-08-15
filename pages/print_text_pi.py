9ea63bb86692a785afa83d62179d23dfb5c45023
zxyctn <zxyctn@gmail.com>
2021-08-15 20:45:58 +0200

    Add comments, print_text_pi and update gitignore

--- pages/print_text_pi.py
+++ pages/print_text_pi.py
@@ -1,6 +1,7 @@
 from escpos.printer import Serial
 
-def print_text(text):
+def print_text_pi(text: str):
+    # Establishing connection
     p = Serial(devfile='/dev/serial0',
             baudrate=19200,
             bytesize=8,
@@ -9,6 +10,7 @@ def print_text(text):
             timeout=1.00,
             dsrdtr=True)
 
+    # Printing configuration
     p.set(align='center', font='b', width=2, height=2)
     p.text("\n\n\n\n" + text + "\n\n\n\n")
     p.cut()
