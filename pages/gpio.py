a789fbc24cbd7e4f6beb668916dc69fe06dcbcf7
zxyctn <zxyctn@gmail.com>
2021-08-19 11:59:17 +0200

    Add debug code to gpio.py

--- pages/gpio.py
+++ pages/gpio.py
@@ -3,7 +3,7 @@ import time
 from random import randrange
 
 servoPIN = 18
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
@@ -12,12 +12,24 @@ try:
   while True:
     p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
+    print('First movement')
+
     p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
+    print('Second movement')
+
+
     p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
+    print('Third movement')
+
+  
     p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
+    print('Last movement')
+
+    print('3 second delay')
+    time.sleep(3)
 
 except KeyboardInterrupt:
   p.stop()
