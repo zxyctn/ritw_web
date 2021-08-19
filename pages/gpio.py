0c66f55f7460a39878e453bbc9cffd8ffef75263
zxyctn <zxyctn@gmail.com>
2021-08-19 12:15:04 +0200

    Update gpio.py

--- pages/gpio.py
+++ pages/gpio.py
@@ -3,28 +3,28 @@ import time
 from random import randrange
 
 servoPIN = 18
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
 p.start(2.5)  # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
+    p.ChangeDutyCycle(randrange(0, 100, 1))
     time.sleep(randrange(500, 1000, 1)/1000)
     print('First movement')
 
-    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
+    p.ChangeDutyCycle(randrange(0, 100, 1))
     time.sleep(randrange(500, 1000, 1)/1000)
     print('Second movement')
 
 
-    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
+    p.ChangeDutyCycle(randrange(0, 100, 1))
     time.sleep(randrange(500, 1000, 1)/1000)
     print('Third movement')
 
   
-    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
+    p.ChangeDutyCycle(randrange(0, 100, 1))
     time.sleep(randrange(500, 1000, 1)/1000)
     print('Last movement')
 
