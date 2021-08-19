5e232807223424b0dbb27b099a497dc5d2f8f163
zxyctn <zxyctn@gmail.com>
2021-08-19 10:52:18 +0100

    Update gpio.py

--- pages/gpio.py
+++ pages/gpio.py
@@ -10,13 +10,13 @@ p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
 p.start(2.5)  # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(randrange(0, 0.87, 0.01))
+    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
-    p.ChangeDutyCycle(randrange(0, 0.87, 0.01))
+    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
-    p.ChangeDutyCycle(randrange(0, 0.87, 0.01))
+    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
-    p.ChangeDutyCycle(randrange(0, 0.87, 0.01))
+    p.ChangeDutyCycle(randrange(0, 87, 1)/100)
     time.sleep(randrange(500, 1000, 1)/1000)
 
 except KeyboardInterrupt:
