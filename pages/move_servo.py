07fdda8aebec2af86778d8d49ae21eae0c503fee
zxyctn <zxyctn@gmail.com>
2021-08-24 13:10:13 +0200

    Add new servo code and webpage details

---
+++ pages/move_servo.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+from random import randrange
+
+servoPIN = 18
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
+p.start(0) 
+
+def SetAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(servoPIN, True)
+    p.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(servoPIN, False)
+    p.ChangeDutyCycle(0)
