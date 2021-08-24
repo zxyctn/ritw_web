51a25839beb60bafaed75d6594c1ddd0749496ba
zxyctn <zxyctn@gmail.com>
2021-08-24 09:54:49 +0200

    Fix printing bug

--- pages/print_text_pi.py
+++ pages/print_text_pi.py
@@ -14,11 +14,7 @@ def print_text_pi(text: str):
                dsrdtr=True)
 
     # Flipping and re-ordering the text fragments
-    text_list = textwrap.wrap(text, width=11,  initial_indent="", subsequent_indent="",
-                              expand_tabs=True, replace_whitespace=True,
-                              fix_sentence_endings=False, break_long_words=True,
-                              drop_whitespace=True, break_on_hyphens=True, tabsize=8,
-                              max_lines=None)
+    text_list = textwrap.wrap(text, width=12,  initial_indent="", subsequent_indent="", max_lines=None)
 
     # Reversing the list to print fragments in the correct order
     text_list.reverse()
@@ -30,10 +26,10 @@ def print_text_pi(text: str):
     p.set(align='center', font='b', width=3,
           flip=True, height=3, custom_size=True)
 
-    p.block_text('\n\n\n', columns=12)
+    p.block_text('\n\n\n', columns=13)
 
     for t in text_list:
-        p.block_text(t + ' ', columns=12)
+        p.block_text(t + ' ', columns=13)
 
-    p.block_text('\n\n\n', columns=12)
+    p.block_text('\n\n\n', columns=13)
     p.cut()
