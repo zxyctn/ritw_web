a642ed78ce275fbc7c0439e614d848d4246fa18e
zxyctn <zxyctn@gmail.com>
2021-08-24 09:42:20 +0200

    Add debug code

--- pages/print_text_pi.py
+++ pages/print_text_pi.py
@@ -13,17 +13,20 @@ def print_text_pi(text: str):
                timeout=1.00,
                dsrdtr=True)
 
-	# Flipping and re-ordering the text fragments
+    # Flipping and re-ordering the text fragments
     text_list = textwrap.wrap(text, width=12,  initial_indent="", subsequent_indent="",
                               expand_tabs=True, replace_whitespace=True,
                               fix_sentence_endings=False, break_long_words=True,
                               drop_whitespace=True, break_on_hyphens=True, tabsize=8,
                               max_lines=None)
 
-	# Reversing the list to print fragments in the correct order
+    # Reversing the list to print fragments in the correct order
     text_list.reverse()
 
-	# Printing configuration
+    # Debug code
+    print(text_list)
+
+    # Printing configuration
     p.set(align='center', font='b', width=3,
           flip=True, height=3, custom_size=True)
 
