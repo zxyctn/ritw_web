4660f4b29e4f3e8f480ed7b1fe04a45395b4410b
zxyctn <zxyctn@gmail.com>
2021-09-16 12:17:59 +0200

    Fix bug

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('print_text', views.print_text, name='print_text')
+    path('print_text', views.print_text, name='print_text'),
+    path('move_servo', views.move_servo, name='move_servo')
 ]
